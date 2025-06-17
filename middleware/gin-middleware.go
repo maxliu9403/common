9a4adf26d236280a843acd0e5854086af2020f2d
刘祥 <liuxiang@mac-2.local>
2025-06-17 15:09:03 +0800

    feat: 弃用Action模式

--- middleware/gin-middleware.go
+++ middleware/gin-middleware.go
@@ -4,15 +4,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-
-	"github.com/maxliu9403/common/gadget"
-	"github.com/maxliu9403/common/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 	"github.com/opentracing/opentracing-go/log"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/maxliu9403/common/logger"
 )
 
 type httpReqResLog struct {
@@ -22,7 +23,7 @@ type httpReqResLog struct {
 	Params     string `json:"params"`
 	Client     string `json:"client"`
 	StatusCode int    `json:"status_code"`
-	Response   string `json:"response"`
+	Response   string `json:"response,omitempty"`
 }
 
 type bodyLogWriter struct {
@@ -30,69 +31,41 @@ type bodyLogWriter struct {
 	body *bytes.Buffer
 }
 
-func (b bodyLogWriter) Write(bs []byte) (int, error) {
-	b.body.Write(bs)
-	return b.ResponseWriter.Write(bs)
+func (w *bodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
 }
 
-const (
-	TheAction = `Action`
-)
-
-func GinInterceptorWithTrace(tra opentracing.Tracer, isResponse bool, ignoreActions ...string) gin.HandlerFunc { //nolint:funlen
+func GinInterceptor(logResponse bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		params := make(map[string]interface{})
-		_ = c.Request.ParseForm()
-		var action string
+		var (
+			bodyBytes []byte
+			params    []byte
+		)
 
-		for k, v := range c.Request.Form {
-			params[k] = v
-			// 忽略不需要的路由trace
-			if k == TheAction {
-				for _, u := range ignoreActions {
-					if v[0] == u {
-						return
-					}
-				}
-				action = v[0]
-			}
-		}
-
-		var span opentracing.Span
-		if tra != nil {
-			spanCtx, err := tra.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
-			if err != nil {
-				span = tra.StartSpan(fmt.Sprintf("%s_%s_%s", c.Request.Method, c.Request.URL.Path, action))
-			} else {
-				span = tra.StartSpan(fmt.Sprintf("%s_%s_%s", c.Request.Method, c.Request.URL.Path, action), opentracing.ChildOf(spanCtx))
-			}
-			defer span.Finish()
-
-			newCtx := opentracing.ContextWithSpan(c, span)
-
-			c.Set(gadget.SpanCtxKey, newCtx)
-		}
-
-		for k, v := range c.Request.PostForm {
-			params[k] = v
-		}
-
-		par, _ := json.Marshal(params)
-
-		var bodyBytes []byte
+		// 读取 body（JSON body / Form body）
 		if c.Request.Body != nil {
-			bodyBytes, _ = ioutil.ReadAll(c.Request.Body)
+			bodyBytes, _ = io.ReadAll(c.Request.Body)
+			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 		}
-		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
 		if len(bodyBytes) > 0 {
-			par = bodyBytes
+			params = bodyBytes
+		} else {
+			_ = c.Request.ParseForm()
+			paramsMap := make(map[string]interface{})
+			for k, v := range c.Request.Form {
+				paramsMap[k] = v
+			}
+			params, _ = json.Marshal(paramsMap)
 		}
 
 		lg := &httpReqResLog{
 			Operator: getRequestUser(c.Request.Header),
-			URI:      c.Request.URL.Path, Method: c.Request.Method,
-			Params: string(par), Client: c.ClientIP(),
+			URI:      c.Request.RequestURI,
+			Method:   c.Request.Method,
+			Params:   string(params),
+			Client:   c.ClientIP(),
 		}
 
 		blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
@@ -100,21 +73,97 @@ func GinInterceptorWithTrace(tra opentracing.Tracer, isResponse bool, ignoreActi
 		c.Next()
 
 		lg.StatusCode = c.Writer.Status()
-		if isResponse {
+		if logResponse {
 			lg.Response = blw.body.String()
 		}
 
 		logBytes, _ := json.Marshal(&lg)
 		logger.Debugf("request details: %s", string(logBytes))
+	}
+}
+
+func GinInterceptorWithTrace(tra opentracing.Tracer, logResponse bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var (
+			bodyBytes []byte
+			err       error
+			params    []byte
+		)
+
+		if c.Request.Body != nil {
+			bodyBytes, _ = io.ReadAll(c.Request.Body)
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		}
+
+		if len(bodyBytes) > 0 {
+			params = bodyBytes
+		} else {
+			_ = c.Request.ParseForm()
+			paramsMap := make(map[string]interface{})
+			for k, v := range c.Request.Form {
+				paramsMap[k] = v
+			}
+			params, _ = json.Marshal(paramsMap)
+		}
+
+		// Tracing span
+		var span opentracing.Span
+		if tra != nil {
+			var spanCtx opentracing.SpanContext
+			spanCtx, err = tra.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
+			opName := fmt.Sprintf("%s_%s", c.Request.Method, c.Request.URL.Path)
+			if err != nil {
+				span = tra.StartSpan(opName)
+			} else {
+				span = tra.StartSpan(opName, opentracing.ChildOf(spanCtx))
+			}
+			defer span.Finish()
+			ext.Component.Set(span, "Gin")
+			ext.SpanKindRPCServer.Set(span)
+
+			c.Set("opentracing-context", opentracing.ContextWithSpan(c, span))
+		}
+
+		// 响应日志封装
+		blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+		c.Writer = blw
+		c.Next()
+
+		lg := &httpReqResLog{
+			Operator:   getRequestUser(c.Request.Header),
+			URI:        c.Request.URL.Path,
+			Method:     c.Request.Method,
+			Params:     string(params),
+			Client:     c.ClientIP(),
+			StatusCode: c.Writer.Status(),
+		}
+		if logResponse {
+			lg.Response = blw.body.String()
+		}
 
 		if span != nil {
 			span.LogFields(
-				log.String("uri", lg.URI), log.String("method", lg.Method),
-				log.String("client", c.ClientIP()), log.String("params", lg.Params),
-				log.Int("code", lg.StatusCode), log.String("response", blw.body.String()),
+				log.String("uri", lg.URI),
+				log.String("method", lg.Method),
+				log.String("params", lg.Params),
+				log.Int("status_code", lg.StatusCode),
 			)
 		}
+
+		logBytes, _ := json.Marshal(&lg)
+		logger.Debugf("request details: %s", string(logBytes))
 	}
+}
+
+func getRequestUser(header http.Header) string {
+	if re, ok := header["X-Forwarded-User"]; ok {
+		return re[0]
+	}
+	if re, ok := header["Authorization"]; ok && len(re) > 0 {
+		// 简单脱敏处理 token
+		return strings.Split(re[0], " ")[0] + ":***"
+	}
+	return ""
 }
 
 func Cors() gin.HandlerFunc {
@@ -128,92 +177,8 @@ func Cors() gin.HandlerFunc {
 
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
+			return
 		}
-
 		c.Next()
-	}
-}
-
-func GinFormatterLog() gin.HandlerFunc {
-	return gin.LoggerWithFormatter(func(params gin.LogFormatterParams) string {
-		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s %d \"%s\" \"%s\" \"\n",
-			params.ClientIP,
-			params.TimeStamp.Format(time.RFC1123),
-			params.Method,
-			params.Path,
-			params.Request.Proto,
-			params.StatusCode,
-			params.Latency,
-			params.BodySize,
-			params.Request.UserAgent(),
-			params.ErrorMessage,
-		)
-	})
-}
-
-func getRequestUser(header http.Header) string {
-	if re, ok := header["X-Forwarded-User"]; ok {
-		return re[0]
-	}
-
-	return ""
-}
-
-// GinInterceptor 用于拦截请求和响应并也写入日志
-func GinInterceptor(isResponse bool, ignoreAction ...string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		params := make(map[string]interface{})
-		ignore := false
-
-		_ = c.Request.ParseForm()
-		for k, v := range c.Request.Form {
-			params[k] = v
-			if k == TheAction {
-				for _, u := range ignoreAction {
-					if v[0] == u {
-						ignore = true
-					}
-				}
-			}
-		}
-
-		for k, v := range c.Request.PostForm {
-			params[k] = v
-		}
-
-		var par []byte
-
-		if !ignore {
-			par, _ = json.Marshal(params)
-			var bodyBytes []byte
-			if c.Request.Body != nil {
-				bodyBytes, _ = ioutil.ReadAll(c.Request.Body)
-			}
-			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
-			if len(bodyBytes) > 0 {
-				par = bodyBytes
-			}
-		}
-
-		lg := &httpReqResLog{
-			Operator: getRequestUser(c.Request.Header),
-			URI:      c.Request.RequestURI,
-			Method:   c.Request.Method,
-			Params:   string(par),
-			Client:   c.ClientIP(),
-		}
-
-		blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
-		c.Writer = blw
-		c.Next()
-
-		lg.StatusCode = c.Writer.Status()
-		if isResponse {
-			lg.Response = blw.body.String()
-		}
-
-		logBytes, _ := json.Marshal(&lg)
-		logger.Debugf("request details: %s", string(logBytes))
 	}
 }
